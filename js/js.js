2c4b5d78964e1151daf1b9f2221189f03bf41eef
zukameku <lilkraceboy@gmail.com>
2021-03-12 23:18:11 +0800

    new

--- js/js.js
+++ js/js.js
@@ -1,11 +1,11 @@
-("#search-icon").click(function() {
-    (".nav").toggleClass("search");
-    (".nav").toggleClass("no-search");
-    (".search-input").toggleClass("search-active");
+$("#search-icon").click(function() {
+    $(".nav").toggleClass("search");
+    $(".nav").toggleClass("no-search");
+    $(".search-input").toggleClass("search-active");
   });
   
-  ('.menu-toggle').click(function(){
-     (".nav").toggleClass("mobile-nav");
-     (this).toggleClass("is-active");
+  $('.menu-toggle').click(function(){
+     $(".nav").toggleClass("mobile-nav");
+     $(this).toggleClass("is-active");
   });
   
